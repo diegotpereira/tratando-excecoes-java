c564a245432d1dde0a9e1215a34b40583a25dc2c
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-01-05 23:56:19 -0300

    criação do app console java com exemplo de tratamento de exceções.

--- src/TesteErroDivisao.java
+++ src/TesteErroDivisao.java
@@ -2,9 +2,27 @@ import java.util.Scanner;
 
 public class TesteErroDivisao {
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-
-        System.out.println("Digite o valor do dividendo: ");
         
+        try {
+            Scanner teclado = new Scanner(System.in);
+
+            System.out.println("Digite o valor do dividendo: ");
+            int numero1 = teclado.nextInt();
+
+            System.out.println("Digite o valor do divisor: ");
+            int numero2 = teclado.nextInt();
+
+            TesteErroDivisao testeErroDivisao = new TesteErroDivisao();
+
+            System.out.println("Resto: " + testeErroDivisao.restoDaDivisao(numero1, numero2));
+        } catch (ErroDivisao e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public int restoDaDivisao(int dividendo, int divisor) throws ErroDivisao {
+        if (divisor > dividendo) {
+            throw new ErroDivisao();
+        }
+        return dividendo % divisor;
     }
 }
